5c55d56c15b61a19b6d3c14920752cc0d316fd25
KentaTokuhara <pokafis.dti408@gmail.com>
2019-12-01 13:21:40 +0900

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,16 @@ User.create!(name:  "Example User",
 name  = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
-User.create!(name:  name,
-      email: email,
-      password:              password,
-      password_confirmation: password,
-      activated: true,
-      activated_at: Time.zone.now)
+# User.create!(name:  name,
+#       email: email,
+#       password:              password,
+#       password_confirmation: password,
+#       activated: true,
+#       activated_at: Time.zone.now)
+
+User.create!( name:   "Example User",
+    email:  "example@railstutorial.org",
+    password: "foobar",
+    password_confirmation: "foobar",
+    admin: true )
 end
